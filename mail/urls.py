9ee930c949e51ca63b119c726eb54b4ec5b10961
melinafallahpour <melinafallahpour79@gmail.com>
2023-09-30 16:58:32 +0330

    m

---
+++ mail/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+
+    # API Routes
+    path("emails", views.compose, name="compose"),
+    path("emails/<int:email_id>", views.email, name="email"),
+    path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
